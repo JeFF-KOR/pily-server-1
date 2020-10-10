bb6d927103b68c0a6168c9fcbb947a02874626d1
JeFF <wjdrlrkdl3@gmail.com>
2020-10-10 21:56:42 +0900

    first init

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200).json("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port} port...`);
+});
