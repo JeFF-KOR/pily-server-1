5bb818d7c647b6c6647239dac44d83273f454255
JeFF <wjdrlrkdl3@gmail.com>
2020-10-13 14:31:54 +0900

    completed session, cors

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+const session = require("express-session");
+const cors = require("cors");
+
+app.use(session({
+  secret: process.env.SECRET,
+  cookie: { secure: false }
+
+}));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json("Hello, World!");
