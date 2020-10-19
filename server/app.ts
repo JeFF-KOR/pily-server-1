b24f1bbe3d875754fed61b843feb5a8f871536ca
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-10-19 16:22:15 +0900

    passport관련 작업
    signin, signout, signup등의 API와 passport의 활용.

--- server/app.ts
+++ server/app.ts
@@ -4,28 +4,87 @@ const app = express();
 const port = process.env.PORT;
 import session = require("express-session");
 import cors = require("cors");
+import bodyparser = require('body-parser');
+import passport  from './controllers/passport'
+import signin from './routes/signin'
+import oauth from './routes/oauth'
 
-import DB = require('./models');
+import DB from './models';
+import { Model } from "sequelize/types";
+import { Mode } from "fs";
 const { User } = DB
+const social_type = {
+  google: 1,
+  kakao: 2,
+  naver: 3
+}
 
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 app.use(session({
   secret: process.env.SECRET,
-  cookie: { secure: false }
-
+  cookie: { secure: false },
+  resave: false,
+  saveUninitialized: false
 }));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
-  res.status(200).json("Hello, World!");
+  res.status(200).send(`Hello, World!`);
 });
+
+app.use('/signin', signin);
+app.use('/oauth', oauth);
+app.get('/signout', (req, res) => {
+  req.logout();
+  res.redirect('//localhost:3000/sign');
+})
+app.post('/signup', async (req, res) => {
+  let username:string = req.body.username
+  if (req.user) {
+    let info:any = {info: {}, ...req.user}.info
+    console.log(info)
+    let temp:number[] = [];
+    if (await User.findOne({
+      where: {
+        username
+      }
+    })) {
+      temp.push(1);
+    }
+    if (await User.findOne({
+      where: {
+        social_type: social_type[info.provider],
+        social_id: info.id
+      }
+    })) {
+      temp.push(1);
+    }
+    let result:Model;
+    if (temp.length === 0) {
+      result = await User.create({
+          social_type: social_type[info.provider],
+          social_id: info.id,
+          username
+      });
+    }
+    if (result) {
+      res.status(200).send();
+    } else {
+      res.status(409).send();
+    }
+  } else {
+    res.status(400).send();
+  }
+})
 
 app.listen(port, () => {
   console.log(`Listening on ${port} port...`);
-  User.create({
-    social_type: 2,
-    social_id: '1234141234',
-    username: 'pig-cola'
-  })
 });
 
 console.log();
