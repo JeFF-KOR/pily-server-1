b475a9ec651f7542cfb295e247728c51c49220ba
JeFF <wjdrlrkdl3@gmail.com>
2020-10-20 15:44:52 +0900

    getUser, gerProfile
    내 프로필, 타인의 프로필 조회기능을 구현하였습니다.
    닉네임 중복검사 기능도 구현완료

--- server/controllers/user/getProfile.ts
+++ server/controllers/user/getProfile.ts
@@ -1,22 +1,18 @@
 import db from '../../models';
 import { Request, Response } from "express";
 const { User, Magazine } = db;
-
+// 남의 프로필 검색
 const getProfile = async (req: Request, res: Response) => {
-  try {
-    const getProfiles = await User.findOne({ where: { id: req.params.id } });
-    const getUserMagazine = await Magazine.findAll({ where: { user_id: req.params.id } });
-    let result = JSON.parse(JSON.stringify(getProfiles)); // *
-    console.log(result);
-    result.results = getUserMagazine;
+  const getProfiles = await User.findOne({ where: { username: req.params.username } });
+  let result = JSON.parse(JSON.stringify(getProfiles)); // *
+  console.log(result);
+  const getUserMagazine = await Magazine.findAll({ where: { user_id: result.id } });
+  result.results = getUserMagazine;
 
-    if (!getProfiles) {
-      res.status(404).json({ message: "Not found." });
-    } else {
-      res.status(200).json(result); /* send로 보낼때 JSON.stringify를 해주면된다. json 규칙, send로 보내면 text()를 쓴 뒤 parse를 다시해줘야함 */
-    }
-  } catch (err) {
-    console.error(err);
+  if (!getProfiles) {
+    res.status(404).json({ message: "Not found." });
+  } else {
+    res.status(200).json(result); /* send로 보낼때 JSON.stringify를 해주면된다. json 규칙, send로 보내면 text()를 쓴 뒤 parse를 다시해줘야함 */
   }
 }
 
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -3,9 +3,14 @@ const route = express.Router();
 import getProfile from "../controllers/user/getProfile";
 import getUser from "../controllers/user/getUser";
 import specifyUserName from "../controllers/user/specifyUserName";
+// import magazine from "../controllers/magazine/like";
 
-route.get("/search/", getUser);
-route.get("/search/:id", getProfile);
+route.get("/search", getUser);
+route.get("/search/:username", getProfile);
 route.get("/user/nickname/:username", specifyUserName);
+
+// route.post("/:id/like", likes);
+// route.post("/:id/unlike", unlikes);
+// route.post("/:id/search/like", likeList);
 
 export default route;
