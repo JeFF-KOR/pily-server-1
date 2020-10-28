8030d74a8f63c39f1c68750ced44fbdaabc5e0ee
JeFF <wjdrlrkdl3@gmail.com>
2020-10-28 17:41:33 +0900

    subscribe
    subscribe implemented

--- routes/user.ts
+++ routes/user.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 const route = express.Router();
 import { getProfile, getUser, specifyUserName } from '../controllers/user';
 import likeController from '../controllers/user/likes';
+import subscribeController from "../controllers/user/subscribe";
 import update from './user-update';
 
 route.use('/update', update);
@@ -10,5 +11,7 @@ route.get("/profile/:username", getProfile);
 route.get("/available/:username", specifyUserName);
 route.post("/like", likeController.like);
 route.post("/unlike", likeController.unlike);
+route.post("/subscribe", subscribeController.subscribe);
+route.post("/unsubscribe", subscribeController.unSubscribe);
 
 export default route;
