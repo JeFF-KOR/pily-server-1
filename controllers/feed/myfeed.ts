6cafb5a2982479808cabafb9e0b8d40823b90c8e
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-10-30 23:41:58 +0900

    pull을 위하여 commit

--- controllers/feed/myfeed.ts
+++ controllers/feed/myfeed.ts
@@ -1,18 +1,61 @@
+import { Model, Sequelize, WhereOptions } from "sequelize/types";
 import db from "../../models";
 import { expressFn, user } from "../helper";
 
 const { Feed } = db;
 
-export const myFeed:expressFn = async (req, res) => {
+interface query {
+  offset?: string | number;
+  date?: string;
+  query?: string;
+  page?: string | number;
+}
+
+export const myFeed: expressFn = async (req, res) => {
   const user = <user>req.user;
+  const query = <query>req.query;
+  
+  if (!query.offset) {
+    return res.status(400).send();
+  }
   
   if (!(user && user.exist)) {
     return res.status(404).send();
   }
+  
+  query.page = query.page ? Number(query.page) : 1;
+  query.offset = Number(query.offset);
 
-  const result = await Feed.findAll({
-    where: {
+  if (Number.isNaN(query.page) || Number.isNaN(query.offset)) {
+    res.status(400).send();
+  }
+
+  let where:WhereOptions = {};
+  const sequelize = <typeof Sequelize>db.sequelize;
+
+  if (query.query) {
+    where = {
+      user_id: user.userInfo.id,
+      title: sequelize.fn('lower', sequelize.col('title'), 'like', `%${query.query.toLowerCase()}%`),
+      subTitle: sequelize.fn('lower', sequelize.col('subTitle'), 'like', `%${query.query.toLowerCase()}%`)
+    }
+  } else {
+    where = {
       user_id: user.userInfo.id
     }
+  }
+
+  const result = await Feed.findAll({
+    where
   })
-} 
+
+  let results: Model[];
+
+  for (let i = 0; i < result.length; i++) {
+    if ((query.page - 1) * query.offset <= i && i < query.page * query.offset) {
+      results.push(result[i]);
+    }
+  }
+
+  res.status(200).json({results, max_count: result.length})
+}
--- routes/feed.ts
+++ routes/feed.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
-import { createFeed, imgUpload } from '../controllers/feed';
+import { createFeed, imgUpload, myFeed } from '../controllers/feed';
 
 const route = express.Router();
 
 route.post('/create', createFeed);
 route.post('/img-upload', imgUpload);
-
+route.get('/my-feed', myFeed)
 
 
 export default route;
