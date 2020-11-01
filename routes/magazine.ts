28f86b42ff874be2d2ac82b477ca753eadb922b2
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-11-01 18:07:16 +0900

    complete magazine-detail

--- routes/magazine.ts
+++ routes/magazine.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
-import { createMagazine, getMagazine, myMagazine } from '../controllers/magazine';
+import { createMagazine, getMagazine, myMagazine, detail } from '../controllers/magazine';
 
 const route = express.Router();
 
 route.post('/create', createMagazine);
 route.get('/mymagazine', myMagazine);
 route.get('/get-magazine', getMagazine);
+route.get('/detail/:id', detail);
 
 export default route;
